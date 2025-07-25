bfed059c715e75e4e4d51b3c83656848457b2a15
Rifat Katranci <rifatkatrani19@gmail.com>
2025-07-25 18:10:11 -0400

    Deleting useless functions that I pasted to debug

--- Test/source/utility/d3dUtil.h
+++ Test/source/utility/d3dUtil.h
@@ -78,10 +78,4 @@ namespace d3dUtil
 		UINT _byteSize = 0u;
 		bool _isConstantBuffer = false;
 	};
-
-	Microsoft::WRL::ComPtr<ID3DBlob> CompileShader(
-		const std::wstring& filename,
-		const D3D_SHADER_MACRO* defines,
-		const std::string& entrypoint,
-		const std::string& target);
 }
